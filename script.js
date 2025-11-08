fad562ac9b5afb9b13e7a767600dd25e47f80fcb
Dimio-AoU <shaqaou@gmail.com>
2025-11-08 23:44:49 +0000

    Add click event listener to title with popup alert

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("Hello from JavaScript!");
+console.log("The Ashes of Unity script is working.");
+const title = document.querySelector("h1");
+title.addEventListener("click", function() {alert("The battle begins!");
+});
