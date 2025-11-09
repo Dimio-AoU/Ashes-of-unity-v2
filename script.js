57c9ae35e84795a2c8dadd28f15686d28bae291e
Dimio-AoU <shaqaou@gmail.com>
2025-11-09 00:05:40 +0000

    Add CSS transition to h1 for smooth color and glow changes

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ console.log("The Ashes of Unity script is working.");
 const title = document.querySelector("h1");
 title.addEventListener("click", function() {
     alert("The battle begins!");
-    title.style.color = "red";
+    if (title.style.color === "red") { 
+        title.style.color = "gold"; 
+} else { 
+title.style.color = "red";
+}
 });
