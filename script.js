ed1bc72f36c937f2a12f6d03627e070c4ffbdfed
Dimio-AoU <shaqaou@gmail.com>
2025-11-09 19:23:14 +0000

    Switch to class toggles (is-red/is-gold) for smooth color transitions

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 console.log("The Ashes of Unity script is working.");
 const title = document.querySelector("h1");
+
 title.addEventListener("click", function() {
     alert("The battle begins!");
-    if (title.style.color === "red") { 
-        title.style.color = "gold"; 
-} else { 
-title.style.color = "red";
-}
+    
+    if (title.classList.contains("is-red")) {
+        title.classList.remove("is-red");
+        title.classList.add("is-gold");
+    } else {
+        title.classList.remove("is-gold");
+        title.classList.add("is-red");
+    }
 });
