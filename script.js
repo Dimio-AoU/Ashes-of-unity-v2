e5f9c3a42582f7d2a22f8397da6deeaec16672a8
Dimio-AoU <shaqaou@gmail.com>
2025-11-09 19:32:47 +0000

    Add hover/touch glow on title via class toggling

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log("The Ashes of Unity script is working.");
 const title = document.querySelector("h1");
 
+// Click: toggle between gold and red
 title.addEventListener("click", function() {
     alert("The battle begins!");
     
@@ -12,3 +13,15 @@ title.addEventListener("click", function() {
         title.classList.add("is-red");
     }
 });
+
+// --- Glow behavior (hover/touch) ---
+function addGlow(){ title.classList.add("glow"); }
+function removeGlow(){ title.classList.remove("glow"); }
+
+title.addEventListener("mouseenter", addGlow);
+title.addEventListener("mouseleave", removeGlow);
+
+title.addEventListener("touchstart", () => {
+    addGlow();
+    setTimeout(removeGlow, 600);
+}, { passive: true});
